e609ac0317c327868c921bac16ef1746ad61228c
raxerz <rajath_krishna@yahoo.co.in>
2016-11-14 00:29:26 +0530

    Changes made part 2

--- imdb/application/models.py
+++ imdb/application/models.py
@@ -14,7 +14,7 @@ class User(models.Model):
 
 class UserPreference(models.Model):
     userid = models.ForeignKey("User")  # Field name made lowercase.    
-    genre = models.CharField(db_column='Genre', max_length=255)  # Field name made lowercase.
+    genre = models.CharField(db_column='Genre', max_length=255, blank=True, null=True)  # Field name made lowercase.
     type = models.CharField(db_column='Type', max_length=255)  # Field name made lowercase.
     other = models.CharField(db_column='Other', max_length=255, blank=True, null=True)  # Field name made lowercase.
 
@@ -25,6 +25,7 @@ class UserPreference(models.Model):
 class Items(models.Model):
     type = models.CharField(db_column='Type', max_length=255)  # Field name made lowercase.
     title = models.CharField(db_column='Title', max_length=255)  # Field name made lowercase.
+    genre = models.CharField(db_column='Genre', max_length=255)  # Field name made lowercase.
     synopsis = models.CharField(db_column='Synopsis', max_length=500, blank=True, null=True)  # Field name made lowercase.
     imageurl = models.CharField(db_column='ImageURL', max_length=1000)  # Field name made lowercase.
     day = models.CharField(db_column='Day', max_length=255, blank=True, null=True)  # Field name made lowercase.
--- imdb/application/urls.py
+++ imdb/application/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [ 
 	url(r'^$',TemplateView.as_view(template_name='signin.html')),
+	url(r'^login/$',views.login,name="login"),
+	url(r'^savepreference/$',views.savePreferences,name="savePreferences"),
+	url(r'^getpref/$',views.getPreferences,name="getpref"),
+	url(r'^getallitems/$',views.getItemList,name="getallitems"),
 	]
